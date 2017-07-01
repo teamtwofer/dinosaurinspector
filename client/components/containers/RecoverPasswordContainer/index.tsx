0aa051db91ab3fee6621e2f7b15d244da7456ce6
Ben Bayard <bjbayard@gmail.com>
2017-07-01 16:50:11 -0700

    Complete user authentication and authorization

---
+++ client/components/containers/RecoverPasswordContainer/index.tsx
@@ -0,0 +1,56 @@
+import { autobind } from 'core-decorators';
+import { inject, observer } from 'mobx-react';
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Stores } from '../../../stores';
+import { RecoverPasswordStore } from '../../../stores/recover-password.store';
+import { AccountHeading } from '../../ui/AccountHeading';
+import { CallToAction } from '../../ui/CallToAction';
+import { Input } from '../../ui/Input';
+
+@inject(({ recoverPasswordStore }: Stores) => ({ recoverPasswordStore }))
+@observer
+export class RecoverPasswordContainer extends React.PureComponent<Props, {}> {
+  @autobind
+  async createAccount(
+    e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>
+  ) {
+    e.preventDefault();
+    const {
+      recoverPasswordStore: { create },
+      match: { params: { id } },
+    } = this.props;
+
+    await create(id);
+  }
+
+  render() {
+    const { match } = this.props;
+    const {
+      password,
+      confirmPassword,
+      error,
+      isLoading,
+    } = this.props.recoverPasswordStore;
+    return (
+      <form onSubmit={this.createAccount}>
+        <AccountHeading match={match} />
+        <Input field={password} name="password" type="password" />
+        <Input
+          field={confirmPassword}
+          name="confirm password"
+          type="password"
+        />
+        {error && <p>{error}</p>}
+        <button type="submit" disabled={isLoading} onClick={this.createAccount}>
+          Login
+        </button>
+        <CallToAction match={match} />
+      </form>
+    );
+  }
+}
+
+export interface Props extends RouteComponentProps<{ id: string }> {
+  recoverPasswordStore: RecoverPasswordStore;
+}
