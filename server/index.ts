eeb7c60a82b7200cdbdcbc5f51abe97872b56cb5
Ben Bayard <bjbayard@gmail.com>
2017-08-18 15:30:50 -0700

    Update types

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ import bundle from './bundle-server';
 import { ApplicationModule } from './modules/application.module';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const port = process.env.PORT || 3001;
+const port = parseInt(process.env.PORT || '3001', 10);
 const publicPath = path.resolve(__dirname, '../public');
 
 const server = express();
